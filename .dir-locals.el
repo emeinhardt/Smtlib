387545a9ad7bb2d430fa4e66463288ad673ec273
Eric Meinhardt <ericmeinhardt@gmail.com>
2023-11-21 11:48:20 -0800

    Initial working haskell.nix flake + ghc947 + hls setup

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+(("src"
+  . ((haskell-mode
+       . ((lsp-haskell-server-path
+            . "haskell-language-server")))))
+("tests"
+  . ((haskell-mode
+       . ((lsp-haskell-server-path
+            . "haskell-language-server")))))
+ )
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Smtlib";
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "x86_64-darwin" ] (system:
+    let
+      compiler = "ghc947";
+      overlays = [ haskellNix.overlay
+        (final: prev: {
+          Smtlib =
+            final.haskell-nix.project' {
+              src = ./.;
+              compiler-nix-name = compiler;
+              shell.tools = {
+                cabal = {};
+                hlint = {};
+                haskell-language-server = {};
+              };
+              shell.buildInputs = with pkgs; [
+                just
+              ];
+            };
+        })
+      ];
+      pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+      flake = pkgs.Smtlib.flake {
+      };
+    in flake // {
+      packages.default = flake.packages."Smtlib:lib:Smtlib";
+    });
+  nixConfig = {
+    # This sets the flake to use the IOG nix cache.
+    # Nix should ask for permission before using it,
+    # but remove it here if you do not want it to.
+    extra-substituters = ["https://cache.iog.io"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="];
+    allow-import-from-derivation = "true";
+  };
+}
